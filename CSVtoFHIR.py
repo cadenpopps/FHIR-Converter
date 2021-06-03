384ce69b7b2e500ce46c365437645431d287ba45
Caden Popps <cadenpopps@gmail.com>
2021-06-02 20:38:19 -0500

    added pretty print

--- CSVtoFHIR.py
+++ CSVtoFHIR.py
@@ -7,25 +7,23 @@ import random
 SKIP_FIRST_LINE = True
 # SKIP_FIRST_LINE = False
 
-# INCLUDE_EMPTY_FIELDS = True
-INCLUDE_EMPTY_FIELDS = False
+PRETTY_PRINT = True
+# PRETTY_PRINT = False
+
 
 def Main():
 
     if len(sys.argv) < 2:
-        print("Please provide filename as such: python HL7Parser.py filename")
-        return 0
+        csvFilename = "data/default.csv"
+    else:
+        csvFilename = sys.argv[1]
 
-    csvFilename = ""
-    outputFilename = "defaultOut.json"
-
-    csvFileContents = getFileContents(sys.argv[1])
+    csvFileContents = getFileContents(csvFilename)
     csvData = parseCSV(csvFileContents)
 
     patients = []
 
     for line in csvData:
-        # p = FHIRPatient(random.randint(10000, 1000000))
         p = FHIRPatient(line[0])
         p.setName(line[1], line[2])
         p.setGender(line[3])
@@ -33,8 +31,15 @@ def Main():
         p.setPhone(line[5])
         patients.append(p)
 
-    for patient in patients:
-        print(patient.constructJSON(), end="\n\n")
+
+    for line, patient in zip(csvData, patients):
+        print()
+        if PRETTY_PRINT:
+            print("Patient: " + line[1] + " " + line[2])
+            print(line)
+            print(patient.constructJSON(), end="\n\n")
+        else:
+            print(patient.constructJSON(), end="\n")
 
     return 0
 
